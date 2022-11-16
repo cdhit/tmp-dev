0ad5b8bd7cae88bcd191e7a0365ce5d12e7f2796
chen dong <chendong.jy@gmail.com>
2022-11-17 12:38:24 +1300

    init

--- src/sections/section_items_factory.js
+++ src/sections/section_items_factory.js
@@ -1,7 +1,7 @@
-import React from "react";
+import React from 'react'
 
-export function SectionItemFactory(props) {
-  const { itype, label, item_id, item_handler } = props;
+export function SectionItemFactory (props) {
+  const { itype, label, item_id, item_handler } = props
   return (
     <label>
       {label}
@@ -12,12 +12,12 @@ export function SectionItemFactory(props) {
         onChange={() => item_handler(item_id)}
       ></input>
     </label>
-  );
+  )
 }
 
-export function SectionItemsFactory(props) {
-  const { configs, section_id, section_handler } = props;
-  const item_handler = section_handler(section_id);
+export function SectionItemsFactory (props) {
+  const { configs, section_id, section_handler } = props
+  const item_handler = section_handler(section_id)
   return (
     <>
       {configs.map(({ itype, label }, index) => (
@@ -30,24 +30,53 @@ export function SectionItemsFactory(props) {
         />
       ))}
     </>
-  );
+  )
 }
 
-export function SectionFactory(props) {
-  const {
-    section_handler,
-    section_id,
-    section_configs,
-    section_keys,
-    section_key,
-    section_key_change
-  } = props;
+export function SectionFactory (props) {
+  const sections_configs = new Map(
+    Object.entries({
+      door: [
+        {
+          itype: 'string',
+          label: 'desc'
+        },
+        {
+          itype: 'number',
+          label: 'height'
+        },
+        {
+          itype: 'number',
+          label: 'lintel-height'
+        }
+      ],
+      wall: [
+        {
+          itype: 'string',
+          label: 'desc'
+        },
+        {
+          itype: 'number',
+          label: 'height'
+        },
+        {
+          itype: 'number',
+          label: 'lintel-height'
+        }
+      ]
+    })
+  )
+
+  const { section_handler, section_id, section_key, section_key_change } = props
+
+  const section_keys = Array.from(sections_configs.keys())
+  const section_configs = sections_configs.get(section_key)
 
   return (
     <>
       <select
         value={section_key}
-        onChange={(e) => section_key_change(section_id, e.target.value)}
+        onChange={e => section_key_change(section_id, e.target.value)}
       >
         {section_keys.map((key, index) => (
           <option key={index}> {key} </option>
@@ -59,62 +88,18 @@ export function SectionFactory(props) {
         section_handler={section_handler}
       />
     </>
-  );
+  )
 }
 
-export default function SectionsFactory(props) {
-  const sections_configs = new Map(
-    Object.entries({
-      door: [
-        {
-          itype: "string",
-          label: "desc"
-        },
-        {
-          itype: "number",
-          label: "height"
-        },
-        {
-          itype: "number",
-          label: "lintel-height"
-        }
-      ],
-      wall: [
-        {
-          itype: "string",
-          label: "desc"
-        },
-        {
-          itype: "number",
-          label: "height"
-        },
-        {
-          itype: "number",
-          label: "lintel-height"
-        }
-      ]
-    })
-  );
-
-  const {
-    section_handler,
-    section_id,
-    section_key,
-    section_key_change
-  } = props;
-
-  const section_keys = Array.from(sections_configs.keys());
-  const section_configs = sections_configs.get(section_key);
-  console.log(section_configs);
+export default function SectionsFactory (props) {
+  const { section_handler, section_id, section_key, section_key_change } = props
 
   return (
     <SectionFactory
-      section_keys={section_keys}
       section_key={section_key}
-      section_configs={section_configs}
       section_id={section_id}
       section_handler={section_handler}
       section_key_change={section_key_change}
     />
-  );
+  )
 }
