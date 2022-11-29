ee7f76fd8eef5ccb719106f2eff106ab4a8239ef
chen dong <chendong.jy@gmail.com>
2022-11-29 15:59:01 +1300

    milestone: event test by warp into a new component

--- src/sections/__tests__/section_item.test.js
+++ src/sections/__tests__/section_item.test.js
@@ -1,10 +1,22 @@
-import React from 'react'
-import Enzyme, { shallow } from 'enzyme'
+import React, { useState } from 'react'
+import Enzyme, { shallow, mount } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 import { SectionItemFactory } from '../section_item'
 
 Enzyme.configure({ adapter: new Adapter() })
+// console.error
+// Warning: An update to TestItemFactory inside a test was not wrapped in act(...).
 
+// When testing, code that causes React state updates should be wrapped into act(...):
+
+// act(() => {
+//   /* fire events that update state */
+// });
+// /* assert on the output */
+
+// This ensures that you're testing the behavior the user would see in the browser. Learn more at https://fb.me/react-wrap-tests-with-act
+//     in TestItemFactory (created by WrapperComponent)
+//     in WrapperComponent
 describe('test section item', () => {
   beforeEach(() => {})
 
@@ -18,19 +30,33 @@ describe('test section item', () => {
   })
 
   it('test SectionItemFactory event', () => {
-    function item_handler (item_id, value) {
-      throw `Parameter ${item_id} ${value}`
+    function TestItemFactory (props) {
+      const [state, setState] = useState(0)
+      const [value, setvalue] = useState(0)
+      function onChange (item_id, value) {
+        setState(item_id)
+        setvalue(value)
+      }
+
+      return (
+        <>
+          <SectionItemFactory
+            itype='string'
+            label='desc'
+            item_id={3}
+            item_handler={onChange}
+          />
+          {state}
+          {value}
+        </>
+      )
     }
-    let wrapper = shallow(
-      <SectionItemFactory
-        itype='string'
-        label='desc'
-        id={1}
-        item_handler={item_handler}
-      />
+
+    let wrapper = mount(<TestItemFactory />)
+    wrapper.find('input').prop('onChange')({ target: { value: 123 } })
+    wrapper.update()
+    expect(wrapper.html()).toEqual(
+      '<label>desc<input type="string" name="desc" id="3"></label>3123'
     )
-    expect(
-      wrapper.find('input').prop('onChange')({ target: { value: 123 } })
-    ).toThrow('Parameter undefined 123')
   })
 })
